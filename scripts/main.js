9da7b7336aa5c7c324e69a5d74534bb3360b3727
lyqianlan <ww13639822630@163.com>
2026-08-02 01:00:52 +0800

    Add files via upload
    灵梦可爱捏

---
+++ scripts/main.js
@@ -0,0 +1,33 @@
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/博丽灵梦2.jpg") {
+    myImage.setAttribute("src", "images/bolilingmeng.jpg");
+  } else {
+    myImage.setAttribute("src", "images/博丽灵梦2.jpg");
+  }
+};
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+  const myName = prompt("Please enter your name.");
+  if (!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `Mozilla is cool, ${myName}`;
+  }
+}
+if (!localStorage.getItem("name")) {
+  setUserName();
+} else {
+  const storedName = localStorage.getItem("name");
+  myHeading.textContent = `请支持博丽灵梦, ${storedName}`;
+}
+
+myButton.onclick = function () {
+  setUserName();
+};
